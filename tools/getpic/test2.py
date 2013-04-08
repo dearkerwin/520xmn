938b4a89772b888e2a2bc3effaebf20db4c371a8
kerwin <dear.kerwin@gmail.com>
2013-04-08 23:25:28 +0800

    增加在首页匹配pic和标签

--- tools/getpic/test2.py
+++ tools/getpic/test2.py
@@ -1,24 +1,33 @@
-import MySQLdb 
+# -*- coding: utf-8 -*-
+import sys; 
+if not "./lib" in sys.path:
+    sys.path.append("./lib") 
 
-conn = MySQLdb.connect(host='localhost', user='root',passwd='123456')
+import UrlParser
+import re
 
-cursor = conn.cursor()   
+content = '<div class="column-post"><div class="entry cf" onmousemove="this.childNodes[0].style.display=\'block\';this.childNodes[2].style.display=\'block\'" onmouseout="this.childNodes[0].style.display=\'none\';this.childNodes[2].style.display=\'none\'"><h2 class="entry-title"><a href="http://girl.pare.cn/6829.html" title="链向 谁家的沙发好黄啊 的固定链接" rel="bookmark" target="_blank">谁家的沙发好黄啊</a></h2><a href="http://girl.pare.cn/6829.html" title="谁家的沙发好黄啊" target="_blank"><p><img src="http://pic.pare.cn/p3/2013-04-08-12-19-59_fw580" alt="谁家的沙发好黄啊" /></p>\
+</a><div class="tag-links"><span class="entry-utility-prep entry-utility-prep-tag-links">标签：</span> <a href="http://girl.pare.cn/tag/sofa" rel="tag">ppppppppp</a>, <a href="http://girl.pare.cn/tag/nature" rel="tag">xxxxxx</a>, <a href="http://girl.pare.cn/tag/pink" rel="tag">ttttttttt</a>, <a href="http://girl.pare.cn/tag/legs" rel="tag">美腿</a>, <a href="http://girl.pare.cn/tag/underwaist" rel="tag">背心</a>, <a href="http://girl.pare.cn/tag/face" rel="tag">jjjjjjj</a></div></div>\
+			</div>\
+			<div class="column-post">\
+<div class="entry cf"  onmouseout="this.childNodes[0].style.display=\'none\';this.childNodes[2].style.display=\'none\'"><h2 class="entry-title"><a href="http://girl.pare.cn/6826.html" title="链向 浙江传媒学院-邵星芸 的固定链接" rel="bookmark" target="_blank">浙江传媒学院-邵星芸</a></h2><a href="http://girl.pare.cn/6826.html" title="浙江传媒学院-邵星芸" target="_blank"><p><img src="http://pic.pare.cn/p3/2013-04-08-12-19-01_fw580" alt="浙江传媒学院-邵星芸" /></p>\
+</a><div class="tag-links"><span class="entry-utility-prep entry-utility-prep-tag-links">标签：</span> <a href="http://girl.pare.cn/tag/hairline" rel="tag">发丝</a>, <a href="http://girl.pare.cn/tag/lips" rel="tag">嘴唇</a>, <a href="http://girl.pare.cn/tag/story" rel="tag">情节</a>, <a href="http://girl.pare.cn/tag/hand" rel="tag">手手</a>, <a href="http://girl.pare.cn/tag/nature" rel="tag">清纯</a>, <a href="http://girl.pare.cn/tag/sun" rel="tag">阳光</a></div></div>\
+			</div>\
+			<div class="column-post">\
+<div class="entry cf" onmousemove="this.childNodes[0].style.display=\'block\';this.childNodes[2].style.display=\'block\'" onmouseout="this.childNodes[0].style.display=\'none\';this.childNodes[2].style.display=\'none\'"><h2 class="entry-title"><a href="http://girl.pare.cn/6823.html" title="链向 非诚勿扰-薛璐 的固定链接" rel="bookmark" target="_blank">非诚勿扰-薛璐</a></h2><a href="http://girl.pare.cn/6823.html" title="非诚勿扰-薛璐" target="_blank"><p><img src="http://pic.pare.cn/p3/2013-04-08-12-17-39_fw580" alt="非诚勿扰-薛璐" /></p>\
+</a><div class="tag-links"><span class="entry-utility-prep entry-utility-prep-tag-links">标签：</span> <a href="http://girl.pare.cn/tag/side" rel="tag">侧面</a>, <a href="http://girl.pare.cn/tag/sofa" rel="tag">沙发</a>, <a href="http://girl.pare.cn/tag/bob" rel="tag">短发</a>, <a href="http://girl.pare.cn/tag/legs" rel="tag">美腿</a>, <a href="http://girl.pare.cn/tag/chiffon" rel="tag">薄纱</a></div></div>\
+			</div>'
 
-
-conn.select_db('520xmn');   
-
-# cursor.execute("""create table test(id int, info varchar(100)) """)   
-
-value = [1,"inserted ?"];   
-
-cursor.execute("insert into test values(%s,%s)",value);   
-
-values=[]   
-
-for i in range(20):   
-    values.append((i,'Hello mysqldb, I am recoder ' + str(i)))   
-
-
-cursor.executemany("""insert into test values(%s,%s) """,values);   
-
-cursor.close();  
+# rex = r'''[\s\S]*?column-post[\s\S]*?<img.*?src\s*=\s*["'\s]*([^"']+?)["'\s]+.*?>[\s\S]*?div[\s\S]*?span>([\s\S]*?)div[\s\S]*?div[\s\S]*?div'''
+rex = r'''[\s\S]*?column-post[\s\S]*?<img.*?src\s*=\s*["'\s]*([^"']+?)["'\s]+.*?>[\s\S]*?div[\s\S]*?span>([\s\S]*?)div[\s\S]*?div[\s\S]*?div'''
+url = re.findall(rex, content, re.S|re.I)
+img = []
+for u in url:
+	src = u[0]
+	print src
+	tag = u[1]
+	# print tag
+	rex1 = r'''<a[^>]+?>([^<]+?)</a>'''
+	# rex1 = r'''<a([^<]+?)</a>'''
+	t = re.findall(rex1, tag, re.S|re.I)
+	print t
