b72fbb4f7eaf74813838c6edf814bef787b4eb4a
kerwin <dear.kerwin@gmail.com>
2013-05-18 19:22:44 +0800

    add baidu analytics

--- webapp/protected/views/layouts/ajax.php
+++ webapp/protected/views/layouts/ajax.php
@@ -1,2 +1,9 @@
+<!DOCTYPE html>
+<html lang="en" >
+    <head>
+    </head>
+    <body>
 <?php echo $content;?>
 <?php echo $this->renderPartial('/layouts/analytics'); ?> 
+</body>
+</html>
--- webapp/protected/views/layouts/analytics.php
+++ webapp/protected/views/layouts/analytics.php
@@ -1,1 +1,10 @@
-	<script type="text/javascript" src="http://tajs.qq.com/stats?sId=24638220" charset="UTF-8"></script>
+<script type="text/javascript" src="http://tajs.qq.com/stats?sId=24638220" charset="UTF-8"></script>
+<script type="text/javascript">
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?dba6b32f762bd7d69f6fb74d43760b11";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
